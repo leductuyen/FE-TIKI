24e2335a722766b8b4589c51b5360f170d4a34bc
TuyenLe <leductuyenbs15042001@gmail.com>
2022-07-09 12:34:25 +0700

    update

--- src/Componets/Product/components/ProductDetail/ProductDetail.jsx
+++ src/Componets/Product/components/ProductDetail/ProductDetail.jsx
@@ -1,43 +1,63 @@
 import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { useRouteMatch } from 'react-router-dom';
-import productApi from '../../../../api/product';
-
+import DetailBuy from './DetailBuy/DetailBuy';
+import DetailDescribe from './DetailDescribe/DetailDescribe';
+import { Carousel } from 'react-carousel-minimal';
 import Styles from './ProductDetail.module.scss';
-
+import HomeIcon from '@material-ui/icons/Home';
+import GroupAddIcon from '@material-ui/icons/GroupAdd';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+const data = [
+    {
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg',
+    },
+    {
+        image: 'https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg',
+    },
+    {
+        image: 'https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg',
+    },
+    {
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Palace_of_Fine_Arts_%2816794p%29.jpg/1200px-Palace_of_Fine_Arts_%2816794p%29.jpg',
+    },
+    {
+        image: 'https://i.natgeofe.com/n/f7732389-a045-402c-bf39-cb4eda39e786/scotland_travel_4x3.jpg',
+    },
+    {
+        image: 'https://www.tusktravel.com/blog/wp-content/uploads/2020/07/Best-Time-to-Visit-Darjeeling-for-Honeymoon.jpg',
+    },
+];
+const thumbnails = {
+    width: '64px',
+    height: '64px',
+};
 function ProductDetail(props) {
-    const {
-        params: { productId },
-    } = useRouteMatch();
-
-    const [items, setItems] = useState({});
-
-    useEffect(() => {
-        (async () => {
-            try {
-                const result = await productApi.get(productId);
-                setItems(result);
-            } catch (error) {
-                console.log('Feild', error);
-            }
-        })();
-    }, [productId]);
     return (
         <div className={Styles.Container}>
+            <div className={Styles.BreadCrum}>
+                <div className={Styles.ItemBreadCrum}>
+                    <div className={Styles.Home}>
+                        <div className={Styles.IconsHome}>
+                            <HomeIcon />
+                        </div>
+                        <span className={Styles.DataCrum}>BOO STORE</span>
+                    </div>
+                    <div className={Styles.Home}>
+                        <div className={Styles.Icon}>
+                            <GroupAddIcon />
+                        </div>
+                        <span className={Styles.DataCrum}>Người theo dõi</span>
+                    </div>
+                    <div className={Styles.Home}>
+                        <div className={Styles.Icon}>
+                            <ChatBubbleOutlineIcon />
+                        </div>
+                        <span className={Styles.DataCrum}>Phản hồi chat 100%</span>
+                    </div>
+                </div>
+            </div>
             <div className={Styles.Wrapper}>
                 <div className={Styles.Left}>
-                    <div className={Styles.GroupImages}>
-                        <div className={Styles.Thumbnail}>
-                            <div className={Styles.ItemsImage}>
-                                <img
-                                    src="https://salt.tikicdn.com/cache/400x400/ts/product/15/98/92/e6d0c5d3b51b5f8fd348969e12a8f465.jpg.webp"
-                                    alt=""
-                                />
-                            </div>
-                        </div>
-                    </div>
-                    <div className={Styles.ReviewImages}></div>
+                    <Carousel data={data} width="444px" height="444px" thumbnails={thumbnails} />
                 </div>
                 <div className={Styles.Bottom}></div>
                 <div className={Styles.Right}>
@@ -66,17 +86,159 @@ function ProductDetail(props) {
                                     </div>
                                     <h1>Miễn phí vận chuyển</h1>
                                 </div>
-                                <div></div>
                             </div>
+                            <div className={Styles.SizeChart}>
+                                <button>Bảng kích thước</button>
+                                <img
+                                    src="https://frontend.tikicdn.com/_desktop-next/static/img/pdp_revamp_v2/icon-right.svg"
+                                    alt=""
+                                />
+                            </div>
+                            <div>
+                                <p className={Styles.P}>Màu</p>
+
+                                <div className={Styles.Options}>
+                                    <div className={Styles.Data}>
+                                        <div className={Styles.Figure}>
+                                            <picture>
+                                                <img
+                                                    className={Styles.Img}
+                                                    src="https://salt.tikicdn.com/cache/100x100/ts/product/8e/9f/6d/46190791027a824ab90a91d513acc5e4.png.webp"
+                                                    alt=""
+                                                />
+                                            </picture>
+                                        </div>
+                                        <span className={Styles.Span}>Trắng</span>
+                                        <div>
+                                            <img
+                                                className={Styles.IconStyles}
+                                                src="https://frontend.tikicdn.com/_desktop-next/static/img/pdp_revamp_v2/selected-variant-indicator.svg"
+                                                alt=""
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className={Styles.Data}>
+                                        <div className={Styles.Figure}>
+                                            <picture>
+                                                <img
+                                                    className={Styles.Img}
+                                                    src="https://salt.tikicdn.com/cache/100x100/ts/product/8e/9f/6d/46190791027a824ab90a91d513acc5e4.png.webp"
+                                                    alt=""
+                                                />
+                                            </picture>
+                                        </div>
+                                        <span className={Styles.Span}>Trắng</span>
+                                        <div>
+                                            <img
+                                                className={Styles.IconStyles}
+                                                src="https://frontend.tikicdn.com/_desktop-next/static/img/pdp_revamp_v2/selected-variant-indicator.svg"
+                                                alt=""
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className={Styles.Data}>
+                                        <div className={Styles.Figure}>
+                                            <picture>
+                                                <img
+                                                    className={Styles.Img}
+                                                    src="https://salt.tikicdn.com/cache/100x100/ts/product/8e/9f/6d/46190791027a824ab90a91d513acc5e4.png.webp"
+                                                    alt=""
+                                                />
+                                            </picture>
+                                        </div>
+                                        <span className={Styles.Span}>Trắng</span>
+                                        <div>
+                                            <img
+                                                className={Styles.IconStyles}
+                                                src="https://frontend.tikicdn.com/_desktop-next/static/img/pdp_revamp_v2/selected-variant-indicator.svg"
+                                                alt=""
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className={Styles.Data}>
+                                        <div className={Styles.Figure}>
+                                            <picture>
+                                                <img
+                                                    className={Styles.Img}
+                                                    src="https://salt.tikicdn.com/cache/100x100/ts/product/8e/9f/6d/46190791027a824ab90a91d513acc5e4.png.webp"
+                                                    alt=""
+                                                />
+                                            </picture>
+                                        </div>
+                                        <span className={Styles.Span}>Trắng</span>
+                                        <div>
+                                            <img
+                                                className={Styles.IconStyles}
+                                                src="https://frontend.tikicdn.com/_desktop-next/static/img/pdp_revamp_v2/selected-variant-indicator.svg"
+                                                alt=""
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className={Styles.Data}>
+                                        <div className={Styles.Figure}>
+                                            <picture>
+                                                <img
+                                                    className={Styles.Img}
+                                                    src="https://salt.tikicdn.com/cache/100x100/ts/product/8e/9f/6d/46190791027a824ab90a91d513acc5e4.png.webp"
+                                                    alt=""
+                                                />
+                                            </picture>
+                                        </div>
+                                        <span className={Styles.Span}>Trắng</span>
+                                        <div>
+                                            <img
+                                                className={Styles.IconStyles}
+                                                src="https://frontend.tikicdn.com/_desktop-next/static/img/pdp_revamp_v2/selected-variant-indicator.svg"
+                                                alt=""
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <DetailBuy />
                         </div>
                         <div className={Styles.BodyRight}>
                             <div className={Styles.Title}>
-                                <div>hello</div>
+                                <div className={Styles.Origin}>
+                                    <div>
+                                        <h1>Xuất xứ:</h1>
+                                    </div>
+                                    <div>
+                                        <h1>Việt Nam</h1>
+                                    </div>
+                                </div>
+                                <div className={Styles.Origin}>
+                                    <div>
+                                        <h1>Xuất xứ:</h1>
+                                    </div>
+                                    <div>
+                                        <h1>Việt Nam</h1>
+                                    </div>
+                                </div>
+                                <div className={Styles.Origin}>
+                                    <div>
+                                        <h1>Xuất xứ:</h1>
+                                    </div>
+                                    <div>
+                                        <h1>Việt Nam</h1>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={Styles.Size}>
+                                <p>Size</p>
+                                <div className={Styles.ListSize}>
+                                    <button>2XL</button>
+                                    <button>2XL</button>
+                                    <button>2XL</button>
+                                    <button>2XL</button>
+                                    <button>2XL</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <DetailDescribe />
         </div>
     );
 }
